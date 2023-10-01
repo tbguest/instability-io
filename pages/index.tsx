bef69a6060be4715c9536d6e3d60ac8c169c2e30
Tristan Guest <tristanguest@gmail.com>
2023-10-01 12:56:12 -0300

    Footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
+import { Footer } from "@/components/Footer";
 import { drawPathTranslate } from "@/utils/canvas/draw-path-translate";
 import { evolve, initialize } from "@/utils/models/ripples/lib";
 import { Inter } from "next/font/google";
+import Head from "next/head";
 import React, { useEffect, useRef, useState } from "react";
 
 // I am building a website
@@ -31,40 +33,45 @@ export default function Translate() {
   }, [elevation]);
 
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center p-8 ${inter.className}`}
-    >
-      <div className={`flex flex-col max-w-xl p-4`}>
-        <div className={`flex flex-grow`}>
-          <canvas
-            className={`flex max-w-full`}
-            width={550}
-            height={360}
-            ref={canvasRef}
-          ></canvas>
-          <canvas
-            width={550}
-            height={360}
-            ref={canvasRef2}
-            style={{ display: "none" }}
-          ></canvas>
+    <div className="flex flex-col justify-between min-h-screen">
+      <Head>
+        <title>Instability IO</title>
+        <meta property="og:title" content={"Instability IO"} key="title" />
+      </Head>
+      <main className={`flex flex-col items-center p-8 ${inter.className}`}>
+        <div className={`flex flex-col max-w-xl p-4`}>
+          <div className={`flex flex-grow`}>
+            <canvas
+              className={`flex max-w-full`}
+              width={550}
+              height={360}
+              ref={canvasRef}
+            ></canvas>
+            <canvas
+              width={550}
+              height={360}
+              ref={canvasRef2}
+              style={{ display: "none" }}
+            ></canvas>
+          </div>
+          <section className="flex flex-col gap-4 text-sm">
+            <h1 className="flex items-start font-bold text-xl">
+              Ripples in wind-blown sand
+            </h1>
+            <p>
+              {
+                "A simulated sand bed might take on a 'rippled' state if the conditions are right. The bed evolves through 'saltation' - the downwind movement of individual grains - coupled with localized 'diffusion' that keeps the bed relatively smooth."
+              }
+            </p>
+            <p>
+              {
+                "Grains at higher elevations are transported farther, and grains on leeward slopes aren't transported at all."
+              }
+            </p>
+          </section>
         </div>
-        <section className="flex flex-col gap-4 text-sm">
-          <h1 className="flex items-start font-bold text-xl">
-            Ripples in wind-blown sand
-          </h1>
-          <p>
-            {
-              "A simulated sand bed might take on a 'rippled' state if the conditions are right. The bed evolves through 'saltation' - the downwind movement of individual grains - coupled with localized 'diffusion' that keeps the bed relatively smooth."
-            }
-          </p>
-          <p>
-            {
-              "Grains at higher elevations are transported farther, and grains on leeward slopes aren't transported at all."
-            }
-          </p>
-        </section>
-      </div>
-    </main>
+      </main>
+      <Footer />
+    </div>
   );
 }
